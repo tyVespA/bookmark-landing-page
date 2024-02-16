5b38321c64ff838c6521f1ea78b44674e2bb89ac
tyVespA <migliavacca.marco08@gmail.com>
2024-02-16 13:14:52 +0100

    Create and style
    TODO: add media queries

---
+++ components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import Button from "./Button";
+
+export default function Hero() {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.textContainer}>
+        <h1>A Simple Bookmark Manager</h1>
+        <p>
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div className={styles.buttonsContainer}>
+          <Button backgroundColor="blue">Get it on Chrome</Button>
+          <Button backgroundColor="grey">Get it on Firefox</Button>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src="../images/illustration-hero.svg" alt="" />
+      </div>
+    </div>
+  );
+}
