6b72282e6e038f398568f98e7178c1036c83e99e
tyVespA <migliavacca.marco08@gmail.com>
2024-02-18 12:36:49 +0100

    Add active tabButton logic

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./FeaturesSection.module.css";
 
 export default function FeaturesSection() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <section className={styles.featureSectionContainer}>
       <h2>Features</h2>
@@ -11,9 +18,30 @@ export default function FeaturesSection() {
         them on the go.
       </p>
       <section className={styles.tabsSection}>
-        <button className={styles.tabsButton}>Simple Bookmarking</button>
-        <button className={styles.tabsButton}>Speedy Searching</button>
-        <button className={styles.tabsButton}>Easy Sharing</button>
+        <button
+          className={`${styles.tabsButton} ${
+            activeIndex === 0 ? styles.active : ""
+          }`}
+          onClick={() => handleTabClick(0)}
+        >
+          Simple Bookmarking
+        </button>
+        <button
+          className={`${styles.tabsButton} ${
+            activeIndex === 1 ? styles.active : ""
+          }`}
+          onClick={() => handleTabClick(1)}
+        >
+          Speedy Searching
+        </button>
+        <button
+          className={`${styles.tabsButton} ${
+            activeIndex === 2 ? styles.active : ""
+          }`}
+          onClick={() => handleTabClick(2)}
+        >
+          Easy Sharing
+        </button>
       </section>
     </section>
   );
