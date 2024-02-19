b9730355487eb44c9489f721c6f2fdddec03cb90
tyVespA <migliavacca.marco08@gmail.com>
2024-02-19 14:59:57 +0100

    Add FeatureTab(s) and add visibility logic

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import styles from "./FeaturesSection.module.css";
+import FeatureTab from "./FeatureTab";
 
 export default function FeaturesSection() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -42,6 +43,48 @@ export default function FeaturesSection() {
         >
           Easy Sharing
         </button>
+      </section>
+      <section className={styles.featuresTabsSection}>
+        <div
+          className={`${styles.featureOneContainer} ${
+            activeIndex === 0 ? "visible" : "hidden"
+          }`}
+        >
+          <FeatureTab
+            imgName="illustration-features-tab-1"
+            title="Bookmark in one click"
+          >
+            Organize your bookmarks however you like. Our simple drag-and-drop
+            interface gives you complete control over how you manage your
+            favourite sites.
+          </FeatureTab>
+        </div>
+        <div
+          className={`${styles.featureTwoContainer} ${
+            activeIndex === 1 ? "visible" : "hidden"
+          }`}
+        >
+          <FeatureTab
+            imgName="illustration-features-tab-2"
+            title="Intelligent search"
+          >
+            Our powerful search feature will help you find saved sites in no
+            time at all. No need to trawl through all of your bookmarks.
+          </FeatureTab>
+        </div>
+        <div
+          className={`${styles.featureThreeContainer} ${
+            activeIndex === 2 ? "visible" : "hidden"
+          }`}
+        >
+          <FeatureTab
+            imgName="illustration-features-tab-3"
+            title="Share your bookmarks"
+          >
+            Easily share your bookmarks and collections with others. Create a
+            shareable link that you can send at the click of a button.
+          </FeatureTab>
+        </div>
       </section>
     </section>
   );
