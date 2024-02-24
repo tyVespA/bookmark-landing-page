8f1ad26d9b24b789b1f22c6e31a06b1dec31f2c6
tyVespA <migliavacca.marco08@gmail.com>
2024-02-24 12:41:26 +0100

    Add validation logic, add error message

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FooterBanner.module.css";
 import Button from "./Button";
 
 export default function FooterBanner() {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const value = e.target.value;
+
+    if (!emailRegex.test(value)) {
+      setErrorMessage("Please enter a valid email address");
+    } else {
+      setErrorMessage("");
+    }
+  };
+
   return (
     <section className={styles.footerBannerContainer}>
       <p>35,000+ already joined</p>
       <h2>Stay up-to-date with what we’re doing</h2>
-      <form action="">
-        <input type="email" placeholder="Enter your email address" required />
-        <Button backgroundColor="red">Contact Us</Button>
+      <form action="" onSubmit={handleSubmit} noValidate>
+        <input
+          type="email"
+          value={email}
+          placeholder="Enter your email address"
+          required
+          onChange={handleEmailChange}
+        />
+        {errorMessage && (
+          <div className={styles.errorMessage}>{errorMessage}</div>
+        )}
+        <Button type="submit" backgroundColor="red">
+          Contact Us
+        </Button>
       </form>
     </section>
   );
